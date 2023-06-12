6a7c4a04ac9388033eb391470bbc6626069b73c5
maxprodigy <peterphilipjohnson@gmail.com>
2023-06-12 09:11:15 -0700

    polls

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -22,7 +22,7 @@ int is_palindromeUtil(listint_t *right, listint_t **left);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-def element_at(my_list, idx):
+
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
