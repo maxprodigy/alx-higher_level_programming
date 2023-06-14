385b8b7d87dbb9eb9465014ebe9781963e487bec
maxprodigy <peterphilipjohnson@gmail.com>
2023-06-14 09:20:59 -0700

    kold

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
-!/usr/bin/python3
-
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = [(replace if x == search else x) for x in my_list]
-    return new
+    new_list = []
+    new_list = [x if x != search else replace for x in my_list]
+    return new_list
