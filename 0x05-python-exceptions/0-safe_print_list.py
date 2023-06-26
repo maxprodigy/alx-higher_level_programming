5469fffa7c7bc462dafd5f61d4b803302f5694f8
maxprodigy <peterphilipjohnson@gmail.com>
2023-06-26 16:57:49 -0700

    mode

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(0, x):
-            print("{}".format(my_list[i]), end="")
-            count += 1
-        print()
+        print("{}".format(my_list[i]), end="")
+        count += 1
+    print()
     except:
-        print()
-        return count
+    print()
+    return count
     return count
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    i = 0
-    results = []
-    while (i < list_length):
+    new_list = []
+    for i in range(0, list_length):
         try:
-            a = my_list_1[i]
-            b = my_list_2[i]
-            result = a / b
-        except IndexError:
-            result = 0
-            print("out of range")
+            result = my_list_1[i] / my_list_2[i]
         except TypeError:
-            result = 0
             print("wrong type")
-        except ZeroDivisionError:
             result = 0
+        except ZeroDivisionError:
             print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
         finally:
-            results.append(result)
-        i += 1
-    return(results)
+            new_list.append(result)
+    return new_list
